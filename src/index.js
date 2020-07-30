ba9065c499d816ac4066a8d88440f2cf0c453c5d
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 05:40:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  let taskArray = [];
 
   document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
+    let taskArray = [];
+
     console.log(e.key);
     taskArray.push(e.key);
     console.log(taskArray);
