bbcad40ad7a9b12a030ddfc205589867b256b273
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 05:46:48 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('[type=submit]').addEventListener("click", function(e) {
       li = document.createElement('li')
       document.querySelector('#tasks').appendChild(li).innerHTML = strArray
+      
       e.preventDefault();
     }, false);
 
