55aeec4e89eb71fffdd44c99e21d1ce0daed1856
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 06:31:24 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+
+    document.querySelector('[type=submit]').addEventListener("click", function(e) {
+      li = document.createElement('li')
+      document.querySelector('#tasks').appendChild(li).innerHTML = document.querySelector("input#new-task-description").value
+      document.querySelector("input#new-task-description").value = ""
+      e.preventDefault();
+    }, false);
+
 });
