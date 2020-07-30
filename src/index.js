a26f9e690a774fb200c097437b31d68a0512ffd9
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 06:11:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector('[type=submit]').addEventListener("click", function(e) {
       li = document.createElement('li')
       document.querySelector('#tasks').appendChild(li).innerHTML = document.querySelector("input#new-task-description").value
-
-      console.log(strArray)
       e.preventDefault();
     }, false);
 
