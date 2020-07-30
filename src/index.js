34519e72fa6722b90f60030ba147c861ee2a53bb
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 06:08:28 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  var x = document.getElementById("new-task-description").form.id;
+  var x = document.querySelector("input#new-task-description").form.id;
   console.log(x)
 
     document.querySelector('[type=submit]').addEventListener("click", function(e) {
