ca79ed9dbee0b1c3afde3c9f262135d647706f4a
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 05:51:08 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,26 +1,31 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
-  let taskArray = [];
-
-  document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
-    console.log(e.key);
-    taskArray.push(e.key);
-    console.log(taskArray);
-
-    let strArray = taskArray.join("");
-    console.log("strArray");
-
-    document.querySelector('[type=submit]').addEventListener("click", function(e) {
-      li = document.createElement('li')
-      document.querySelector('#tasks').appendChild(li).innerHTML = strArray
-
-      console.log(strArray)
-      e.preventDefault();
-    }, false);
+  document.querySelector('[type=text]').innerHTML
 
 
-  });
+
+
+  // let taskArray = [];
+  // 
+  // document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
+  //   console.log(e.key);
+  //   taskArray.push(e.key);
+  //   console.log(taskArray);
+  // 
+  //   let strArray = taskArray.join("");
+  //   console.log("strArray");
+  // 
+  //   document.querySelector('[type=submit]').addEventListener("click", function(e) {
+  //     li = document.createElement('li')
+  //     document.querySelector('#tasks').appendChild(li).innerHTML = strArray
+  // 
+  //     console.log(strArray)
+  //     e.preventDefault();
+  //   }, false);
+  // 
+  // 
+  // });
 
 
 });
