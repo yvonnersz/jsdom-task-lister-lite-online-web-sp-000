b16fce080e80396ed84d6c5e4d58bf355ffb707c
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 05:48:34 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", () => {
       li = document.createElement('li')
       document.querySelector('#tasks').appendChild(li).innerHTML = strArray
 
-      console.log(strArray[strArray.length-1])
+      console.log(strArray)
       e.preventDefault();
     }, false);
 
