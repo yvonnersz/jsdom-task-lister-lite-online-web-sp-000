9d34ec1511dfb487a7bfe2b64297fdab724f1d43
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 05:57:35 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", () => {
   document.querySelector('[type=submit]').addEventListener("click", function(e) {
     e.preventDefault();
   }, false);
-  
+
     document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
       let taskArray = [];
 
