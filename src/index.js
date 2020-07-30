2d9e23f53590cf3a2802e69333c77f15532cad85
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 05:39:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,21 +2,23 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
   let taskArray = [];
-  strArray = taskArray.join("");
-
 
   document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
+    console.log(e.key);
     taskArray.push(e.key);
+    console.log(taskArray);
+
+    strArray = taskArray.join("");
+    console.log(strArray);
+
+    li = document.createElement('li')
+    document.querySelector('#tasks').appendChild(li).innerHTML = strArray
   });
 
   document.querySelector('[type=submit]').addEventListener("click", function(e) {
-    li = document.createElement('li')
-
-    console.log(strArray)
-    document.querySelector('#tasks').appendChild(li).innerHTML = strArray
-
-
     e.preventDefault();
+
+    
   }, false);
 
 });
