32197f705aeee1bca61502d7f0656a5da2328e30
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 05:56:03 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,28 +1,53 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
+  document.querySelector('[type=submit]').addEventListener("click", function(e) {
+    e.preventDefault();
 
+    document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
+      let taskArray = [];
 
-  document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
-    let taskArray = [];
+      console.log(e.key);
+      taskArray.push(e.key);
+      console.log(taskArray);
 
-    console.log(e.key);
-    taskArray.push(e.key);
-    console.log(taskArray);
+      let strArray = taskArray.join("");
+      console.log("strArray");
 
-    let strArray = taskArray.join("");
-    console.log("strArray");
-
-    document.querySelector('[type=submit]').addEventListener("click", function(e) {
       li = document.createElement('li')
       document.querySelector('#tasks').appendChild(li).innerHTML = strArray
 
       console.log(strArray)
-      e.preventDefault();
-    }, false);
-
-
-  });
+    
+  }, false);
 
 
 });
+
+
+
+
+
+//   document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
+//     let taskArray = [];
+// 
+//     console.log(e.key);
+//     taskArray.push(e.key);
+//     console.log(taskArray);
+// 
+//     let strArray = taskArray.join("");
+//     console.log("strArray");
+// 
+//     document.querySelector('[type=submit]').addEventListener("click", function(e) {
+//       li = document.createElement('li')
+//       document.querySelector('#tasks').appendChild(li).innerHTML = strArray
+// 
+//       console.log(strArray)
+//       e.preventDefault();
+//     }, false);
+// 
+// 
+//   });
+// 
+// 
+// });
