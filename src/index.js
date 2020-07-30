6d3e6a800024a75c1b8c25496486fbb2f3537a41
Yvonne M Pham <yvonnersz@gmail.com>
2020-07-30 04:57:22 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
 
   document.body.querySelector('[type=text]').addEventListener("keydown", function(e) {
-    if (e !== "Backspace") {
-      console.log(e.key);
+    if (key === "Backspace") {
+      return false;
     }
   });
 });
